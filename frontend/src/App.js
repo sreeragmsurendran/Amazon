1034de6c1cb147326e991e907ee576bad3d594d8
sreeragmsurendran <sreeragmsurendran1997@gmail.com>
2022-03-13 11:51:34 +0530

    firstcommit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <a href="#">Amazone</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
