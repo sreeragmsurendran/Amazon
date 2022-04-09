9494e49d3cdd98116574c77bb3f1704b1b972640
sreeragmsurendran <sreeragmsurendran1997@gmail.com>
2022-04-09 15:16:34 +0530

    fhfhg

--- backend/Routes/seedRoutes.js
+++ backend/Routes/seedRoutes.js
@@ -6,8 +6,8 @@ import User from "../models/userModel.js"
 const seedRouter = express.Router();
 
 seedRouter.get('/',async(req,res)=>{
-    // await Product.remove({});
-    // const createProduct = await Product.insertMany(data.products)
+    await Product.remove({});
+    const createProduct = await Product.insertMany(data.products)
     await User.remove({});
     const createUser= await User.insertMany(data.users);
     res.send({createProduct,createUser});
